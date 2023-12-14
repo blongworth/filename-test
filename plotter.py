dc60f5e1986d073fc5b5b29caadeadef5d2ff3aa
Brett Longworth <blongworth@whoi.edu>
2023-12-14 09:49:33 -0500

    write to file too

--- plotter.py
+++ plotter.py
@@ -7,16 +7,19 @@ plt.ion()  # Turn on interactive mode
 x_data = []
 y_data = []
 
-try:
-    while True:
-        line = ser.readline().decode('utf-8').strip()
-        if ',' in line:
-            y, x = map(int, line.split(','))  # Assuming x and y are ints
-            x_data.append(x)
-            y_data.append(y)
-            plt.scatter(x, y, c='blue')
-            plt.pause(0.1)
-except KeyboardInterrupt:
-    ser.close()
-    plt.ioff()
-    plt.show()
+output_file = open("sd_write_stats.txt", "w")  # Open a file for writing
+with open("sd_write_stats.txt", "w") as output_file:
+    try:
+        while True:
+            line = ser.readline().decode('utf-8').strip()
+            if ',' in line:
+                y, x = map(int, line.split(','))  # Assuming x and y are ints
+                x_data.append(x)
+                y_data.append(y)
+                plt.scatter(x, y, c='blue')
+                plt.pause(0.1)
+                output_file.write(f"{x},{y}\n")
+    except KeyboardInterrupt:
+        ser.close()
+        plt.ioff()
+        plt.show()
